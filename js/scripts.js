c5ab109c89b2459ee3d390da3eab00e9f463ce78
Brian Pritt and Luke Philips <luke@>
2016-11-01 16:21:51 -0700

    add pictures

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,63 @@ Account.prototype.deposit = function(value){
 Account.prototype.withdraw = function(value){
   return this.balance -= value;
 }
+Account.prototype.overdrawn = function(balance) {
+  var status = false;
+  if (this.balance <0 ) {
+   status = true;
+  }
+  return status;
+}
 
+//document ready
 $(document).ready(function(){
+  //create account
   $("form .create-account").click(function(event) {
     event.preventDefault();
 
     var nameInput = $("#name").val();
     var balanceInput = parseInt($("#balance").val());
     newAccount = new Account(nameInput,balanceInput);
-    console.log(newAccount);
+    $(".output").show();
+    $(".customer").text(newAccount.name);
+    $(".balance").text(newAccount.balance);
+    $("#name").val("");
+    $("#balance").val("");
 
   });
+
+  //deposit funds
   $("form .deposit").click(function(event) {
     event.preventDefault();
     var depositInput = parseInt($("#deposit").val());
     newAccount.deposit(depositInput);
-    $(".output").append(newAccount.balance)
+    $(".balance").text(newAccount.balance);
+    $("#deposit").val("");
 
-
+    if (!(newAccount.overdrawn())){
+      $(".balance").removeClass("negBalance")
+      $(".moe").hide();
+    }
   });
+
+  //withdraw funds
   $(".withdraw").click(function(event) {
     event.preventDefault();
-    var withdrawInput = parseInt($("#withdraw").val());
-    newAccount.withdraw(withdrawInput);
-    $(".output").append(newAccount.balance);
+    if (!newAccount.overdrawn()){
+      var withdrawInput = parseInt($("#withdraw").val());
+      newAccount.withdraw(withdrawInput);
+      $(".balance").text(newAccount.balance);
+      $("#withdraw").val("")
 
+
+      if (newAccount.overdrawn()) {
+        $(".balance").addClass("negBalance");
+
+      }
+    } else {
+      $(".moe").show();
+      $("#withdraw").val("")
+    }
   });
 
 });
